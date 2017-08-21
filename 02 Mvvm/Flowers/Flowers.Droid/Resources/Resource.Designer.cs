02d8533453c18835c6ca7d7404f929afde0f12ca
Lbugnion <lbugnion@outlook.com>
2017-08-21 21:58:47 +0200

    Updated assembly info and other meta information

--- 02 Mvvm/Flowers/Flowers.Droid/Resources/Resource.Designer.cs
+++ 02 Mvvm/Flowers/Flowers.Droid/Resources/Resource.Designer.cs
@@ -31,46 +31,12 @@ namespace Flowers
 		public partial class Attribute
 		{
 			
-			// aapt resource value: 0x7f010000
-			public const int layoutManager = 2130771968;
-			
-			// aapt resource value: 0x7f010002
-			public const int reverseLayout = 2130771970;
-			
-			// aapt resource value: 0x7f010001
-			public const int spanCount = 2130771969;
-			
-			// aapt resource value: 0x7f010003
-			public const int stackFromEnd = 2130771971;
-			
 			static Attribute()
 			{
 				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
 			}
 			
 			private Attribute()
-			{
-			}
-		}
-		
-		public partial class Dimension
-		{
-			
-			// aapt resource value: 0x7f040000
-			public const int item_touch_helper_max_drag_scroll_per_frame = 2130968576;
-			
-			// aapt resource value: 0x7f040001
-			public const int item_touch_helper_swipe_escape_max_velocity = 2130968577;
-			
-			// aapt resource value: 0x7f040002
-			public const int item_touch_helper_swipe_escape_velocity = 2130968578;
-			
-			static Dimension()
-			{
-				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
-			}
-			
-			private Dimension()
 			{
 			}
 		}
@@ -94,59 +60,56 @@ namespace Flowers
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f050005
-			public const int AddCommentButton = 2131034117;
-			
-			// aapt resource value: 0x7f050001
-			public const int CommentText = 2131034113;
-			
-			// aapt resource value: 0x7f05000a
-			public const int CommentsList = 2131034122;
-			
 			// aapt resource value: 0x7f050004
-			public const int DescriptionText = 2131034116;
-			
-			// aapt resource value: 0x7f05000d
-			public const int DescriptionTextView = 2131034125;
-			
-			// aapt resource value: 0x7f050007
-			public const int EditNameButton = 2131034119;
-			
-			// aapt resource value: 0x7f050009
-			public const int EditNameText = 2131034121;
-			
-			// aapt resource value: 0x7f050008
-			public const int FlowerImageView = 2131034120;
-			
-			// aapt resource value: 0x7f05000b
-			public const int FlowerTemplateRoot = 2131034123;
-			
-			// aapt resource value: 0x7f05000e
-			public const int FlowersList = 2131034126;
-			
-			// aapt resource value: 0x7f050011
-			public const int LastLoadedText = 2131034129;
-			
-			// aapt resource value: 0x7f050003
-			public const int NameText = 2131034115;
-			
-			// aapt resource value: 0x7f05000c
-			public const int NameTextView = 2131034124;
-			
-			// aapt resource value: 0x7f050010
-			public const int RefreshButton = 2131034128;
-			
-			// aapt resource value: 0x7f050006
-			public const int SaveButton = 2131034118;
-			
-			// aapt resource value: 0x7f050002
-			public const int SaveCommentButton = 2131034114;
-			
-			// aapt resource value: 0x7f05000f
-			public const int Separator = 2131034127;
+			public const int AddCommentButton = 2131034116;
 			
 			// aapt resource value: 0x7f050000
-			public const int item_touch_helper_previous_elevation = 2131034112;
+			public const int CommentText = 2131034112;
+			
+			// aapt resource value: 0x7f050009
+			public const int CommentsList = 2131034121;
+			
+			// aapt resource value: 0x7f050003
+			public const int DescriptionText = 2131034115;
+			
+			// aapt resource value: 0x7f05000c
+			public const int DescriptionTextView = 2131034124;
+			
+			// aapt resource value: 0x7f050006
+			public const int EditNameButton = 2131034118;
+			
+			// aapt resource value: 0x7f050008
+			public const int EditNameText = 2131034120;
+			
+			// aapt resource value: 0x7f050007
+			public const int FlowerImageView = 2131034119;
+			
+			// aapt resource value: 0x7f05000a
+			public const int FlowerTemplateRoot = 2131034122;
+			
+			// aapt resource value: 0x7f05000d
+			public const int FlowersList = 2131034125;
+			
+			// aapt resource value: 0x7f050010
+			public const int LastLoadedText = 2131034128;
+			
+			// aapt resource value: 0x7f050002
+			public const int NameText = 2131034114;
+			
+			// aapt resource value: 0x7f05000b
+			public const int NameTextView = 2131034123;
+			
+			// aapt resource value: 0x7f05000f
+			public const int RefreshButton = 2131034127;
+			
+			// aapt resource value: 0x7f050005
+			public const int SaveButton = 2131034117;
+			
+			// aapt resource value: 0x7f050001
+			public const int SaveCommentButton = 2131034113;
+			
+			// aapt resource value: 0x7f05000e
+			public const int Separator = 2131034126;
 			
 			static Id()
 			{
@@ -189,11 +152,11 @@ namespace Flowers
 		public partial class String
 		{
 			
-			// aapt resource value: 0x7f060001
-			public const int ApplicationName = 2131099649;
+			// aapt resource value: 0x7f040001
+			public const int ApplicationName = 2130968577;
 			
-			// aapt resource value: 0x7f060000
-			public const int Hello = 2131099648;
+			// aapt resource value: 0x7f040000
+			public const int Hello = 2130968576;
 			
 			static String()
 			{
@@ -201,41 +164,6 @@ namespace Flowers
 			}
 			
 			private String()
-			{
-			}
-		}
-		
-		public partial class Styleable
-		{
-			
-			public static int[] RecyclerView = new int[] {
-					16842948,
-					2130771968,
-					2130771969,
-					2130771970,
-					2130771971};
-			
-			// aapt resource value: 0
-			public const int RecyclerView_android_orientation = 0;
-			
-			// aapt resource value: 1
-			public const int RecyclerView_layoutManager = 1;
-			
-			// aapt resource value: 3
-			public const int RecyclerView_reverseLayout = 3;
-			
-			// aapt resource value: 2
-			public const int RecyclerView_spanCount = 2;
-			
-			// aapt resource value: 4
-			public const int RecyclerView_stackFromEnd = 4;
-			
-			static Styleable()
-			{
-				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
-			}
-			
-			private Styleable()
 			{
 			}
 		}
